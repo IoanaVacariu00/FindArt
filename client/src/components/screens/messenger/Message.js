b23af9cbdc432c49935d252b1a05094bd2cae0b8
Ioana Vacariu <ioana.vacariu00@e-uvt.ro>
2023-01-26 13:30:07 +0200

    pliz

--- client/src/components/screens/messenger/Message.js
+++ client/src/components/screens/messenger/Message.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { format } from "timeago.js";
 
 const Message = ()=>{
     return (
   
-        <div className="message">
-            <div className="messageTop"></div>
-            <div className="messageBottom"></div>    
+        <div className={own ? "message own" : "message"}>
+            <div className="messageTop">
+                <img
+                    className="messageImg"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg/1200px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg"
+                    alt=""
+                />
+            
+                <p className="messageText">{message.text}</p>
+            </div>
+            <div className="messageBottom">{format(message.createdAt)}</div>    
         </div>
 
     );
 };
 
 export default Message
-
